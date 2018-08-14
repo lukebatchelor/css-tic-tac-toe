31c02435bf3ec597e84328bf4cc56a63fcd69646
Luke Batchelor <lbatchelor@atlassian.com>
2018-08-14 16:34:02 +1000

    * Prevents making moves after game ended * Prevents calculating impossible games by not searching ended games * Adds X's and O's * Adds reset button * Fixes styles when finding winner * Fixes bug in one of the winning rows (1,5,9) * Slightly prettys up game

--- scripts/build-html.js
+++ scripts/build-html.js
@@ -3,7 +3,6 @@ const path = require('path');
 const outdent = require('outdent');
 
 const outputPath = path.join(__dirname, '..', 'dist', 'index.html');
-const maxDepth = +process.argv.slice(2)[0];
 
 // if we sort the moves in alphabetical order we can ensure that two states
 // that are equivalent become the same state string
@@ -15,7 +14,7 @@ function getWinner(state) {
   const winningSets = [
     [1,2,3], [4,5,6], [7,8,9], // horizontal
     [1,4,7], [2,5,8], [3,6,9], // vertical
-    [1,5,8], [7,5,3]           // vertical
+    [1,5,9], [7,5,3]           // vertical
   ];
   const players = ['r', 'g'];
   const moves = state.split(' ');
@@ -33,21 +32,22 @@ function getWinner(state) {
 }
 
 function getBoardForState(state) {
+  let nextStates;
   const normalisedState = normaliseState(state);
   const numMoves = state.split(' ').filter(Boolean);
   const curPlayer = numMoves.length % 2 === 0 ? 'r' : 'g';
-  const nextStates = [1,2,3,4,5,6,7,8,9].map(possibleMoveIdx => {
+  const winner = getWinner(state);
+
+  nextStates = [1,2,3,4,5,6,7,8,9].map(possibleMoveIdx => {
     // A move is only legal if we haven't seen it before (it wont be in the state string)
-    const isLegalMove = state.indexOf(possibleMoveIdx) === -1;
+    const isLegalMove = state.indexOf(possibleMoveIdx) === -1 && !winner;
     if (isLegalMove) {
       return normaliseState(`${state} ${curPlayer}${possibleMoveIdx}`);
     }
     return normalisedState;
   });
-  const winner = getWinner(state);
-  // if (winner) {
-  //   console.log('Winning state', state)
-  // }
+
+
   const winnerClass = winner ? `winner-${winner}` : '';
   const boardTemplate = outdent`
     <input type="radio" name="game-state" id="${normalisedState}">
@@ -60,22 +60,17 @@ function getBoardForState(state) {
 
 function getAllStates() {
   let allStates = new Set();
-  const curStates = new Set([''])
+  const curStates = new Set(['']);
 
   allStates = getNextStates(allStates, curStates, 0);
-  
+
   return allStates;
 }
 
 function getNextStates(allStates, curStates, depth) {
   const newStates = new Set();
-  console.log(`depth=${depth}, states=${allStates.size}`)
-  if (depth === maxDepth) {
-    console.log('Too deep', allStates.size);
-    process.exit(1)
-  }
-  // console.log('Called getNextStates', allStates.length, curStates)
-  
+  console.log(`depth=${depth}, states=${allStates.size}`);
+
   curStates.forEach(state => {
     const numMoves = state.split(' ').filter(Boolean);
     const curPlayer = numMoves.length % 2 === 0 ? 'r' : 'g';
@@ -95,17 +90,21 @@ function getNextStates(allStates, curStates, depth) {
 }
 
 function getLegalMoves(state) {
+  const winner = getWinner(state);
+  if (winner) {
+    return [];
+  }
   return [1,2,3,4,5,6,7,8,9].filter(move => state.indexOf(move) === -1);
 }
 
-
-// console.log(getBoardForState(''));
-
+const startTime = +new Date();
 const allStates = getAllStates();
-console.log('Found ', allStates.size, 'states');
+const endTime = +new Date();
+
+console.log('Found ', allStates.size, 'states in ', (endTime - startTime), 'milliseconds');
+console.log('Mapping to board states...');
 const allStatesArr = Array.from(allStates);
 const allStateStrings = allStatesArr.map(getBoardForState);
-console.log(allStateStrings.length, 'board states')
 
 const htmlTemplate = outdent`
   <!DOCTYPE html>
@@ -118,22 +117,31 @@ const htmlTemplate = outdent`
     <link href="styles.css" rel="stylesheet"></head>
   </head>
   <body>
-  <input type="radio" name="game-state" id="start" checked>
-  <div class="game">
-    <label for="r1"></label>
-    <label for="r2"></label>
-    <label for="r3"></label>
-    <label for="r4"></label>
-    <label for="r5"></label>
-    <label for="r6"></label>
-    <label for="r7"></label>
-    <label for="r8"></label>
-    <label for="r9"></label>
+  <div class="app">
+    <h1>CSS Tic Tac Toe</h1>
+    <p>This game is built entirely out of HTML and CSS, no JavaScript™ at all!</p>
+    <p>Check out <a href="https://github.com/lukebatchelor/css-tic-tac-toe">lukebatchelor/css-tic-tac-toe</a> if you're interested in how it works!</p>
+    <br><br>
+    <input type="radio" name="game-state" id="start" checked>
+    <div class="game">
+      <label for="r1"></label>
+      <label for="r2"></label>
+      <label for="r3"></label>
+      <label for="r4"></label>
+      <label for="r5"></label>
+      <label for="r6"></label>
+      <label for="r7"></label>
+      <label for="r8"></label>
+      <label for="r9"></label>
+    </div>
+    ${allStateStrings.join('\n')}
+    <label for="start" id="resetButton">Reset</label>
   </div>
-  ${allStateStrings.join('\n')}
-  
   </body>
   </html>
 `;
+console.log('Done!');
+console.log('Writing to', outputPath);
 
 fs.writeFileSync(outputPath, htmlTemplate);
+console.log('Done!');
