141ea42c93f34271bae424a62a602fdd55b5baf9
Luke Batchelor <lbatchelor@atlassian.com>
2018-08-14 16:47:34 +1000

    Adds github banner

--- scripts/build-html.js
+++ scripts/build-html.js
@@ -137,6 +137,7 @@ const htmlTemplate = outdent`
     ${allStateStrings.join('\n')}
     <label for="start" id="resetButton">Reset</label>
   </div>
+  <a href="https://github.com/lukebatchelor/css-tic-tac-toe"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
   </body>
   </html>
 `;
